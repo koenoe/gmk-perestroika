dda2de0d47bc884e3666addef6ea8bd4f0b19528
Lewis Flude <lewisflude@ip-192-168-101-61.eu-west-1.compute.internal>
2020-01-13 15:58:13 +0000

    Add some basic styling to slide counter

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,15 +1,19 @@
 // @flow
 import React from 'react';
-// import styled from '@emotion/styled';
+import styled from '@emotion/styled';
 import ReactImagesCarousel from 'react-images';
 import type { Node } from 'react';
-
 import { Container } from 'components/contentBlocks.js';
-
 import concrete910 from '../../img/concrete-910.png';
 import concreteBauer from '../../img/concrete-bauer.png';
 import concreteOGR from '../../img/concrete-ogr.png';
 import concreteProphet from '../../img/concrete-prophet.png';
+
+const CarouselContainer = styled(Container)`
+  .react-images__footer__count {
+    font-family: 'Montserrat', serif;
+  }
+`;
 
 const images = [
   { src: concrete910 },
@@ -20,8 +24,8 @@ const images = [
 
 export default function Carousel(): Node {
   return (
-    <Container>
+    <CarouselContainer>
       <ReactImagesCarousel views={images} />
-    </Container>
+    </CarouselContainer>
   );
 }
