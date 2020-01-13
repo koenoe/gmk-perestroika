594fd935d52f7a860e7d84c3d8ea3a60705be98f
Koen Romers <info@koenromers.com>
2020-01-13 19:06:52 +0100

    lazyloading carousel images

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
-import styled from '@emotion/styled';
+// import styled from '@emotion/styled';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 import ReactImagesCarousel from 'react-images';
 import type { Node } from 'react';
 
@@ -17,30 +18,34 @@ import officeLynxL50 from '../../img/office-lynxl50.png';
 import officeProphet from '../../img/office-prophet.png';
 
 type Image = {|
-  src: string,
+  source: string,
+  caption: string,
 |};
 
-const CarouselContainer = styled(Container)`
-  .react-images__footer__count {
-    font-family: 'Montserrat', serif;
-  }
-`;
-
 const images: Array<Image> = [
-  { src: concrete910 },
-  { src: concreteBauer },
-  { src: concreteOGR },
-  { src: concreteProphet },
-  { src: officeNoxary280 },
-  { src: officeBauer },
-  { src: officeLynxL50 },
-  { src: officeProphet },
+  { source: concrete910, caption: 'TGR 910 by Yuktsi' },
+  { source: concreteBauer, caption: 'Bauer by Dixie' },
+  { source: concreteOGR, caption: 'OGR by Alchemist Keyboards' },
+  { source: concreteProphet, caption: 'Prophet by Cable Car Designs' },
+  { source: officeNoxary280, caption: 'Noxary 280 by xondat' },
+  { source: officeBauer, caption: 'Bauer by Dixie' },
+  { source: officeLynxL50, caption: 'Lynx L50 by wixxzblu' },
+  { source: officeProphet, caption: 'Prophet by Cable Car Designs' },
 ];
+
+type ViewProps = $ReadOnly<{|
+  data: Image,
+|}>;
+
+function View(props: ViewProps): Node {
+  const { data } = props;
+  return <LazyLoadImage src={data.source} alt={data.caption} effect="blur" />;
+}
 
 export default function Carousel(): Node {
   return (
-    <CarouselContainer>
-      <ReactImagesCarousel views={images} />
-    </CarouselContainer>
+    <Container>
+      <ReactImagesCarousel views={images} components={{ View }} />
+    </Container>
   );
 }
