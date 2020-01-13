53406ce8f081efc839fa35d128523ba020910117
Lewis Flude <lewisflude@ip-192-168-101-61.eu-west-1.compute.internal>
2020-01-13 15:53:15 +0000

    Use Carousel from react-images
    Also, remove lazy loading (for now)

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,24 +1,27 @@
 // @flow
 import React from 'react';
 // import styled from '@emotion/styled';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-
+import ReactImagesCarousel from 'react-images';
 import type { Node } from 'react';
 
 import { Container } from 'components/contentBlocks.js';
 
 import concrete910 from '../../img/concrete-910.png';
-// import concreteBauer from '../../img/concrete-bauer.png';
-// import concreteOGR from '../../img/concrete-ogr.png';
-// import concreteProphet from '../../img/concrete-prophet.png';
+import concreteBauer from '../../img/concrete-bauer.png';
+import concreteOGR from '../../img/concrete-ogr.png';
+import concreteProphet from '../../img/concrete-prophet.png';
+
+const images = [
+  { src: concrete910 },
+  { src: concreteBauer },
+  { src: concreteOGR },
+  { src: concreteProphet },
+];
 
 export default function Carousel(): Node {
   return (
     <Container>
-      <LazyLoadImage src={concrete910} alt="" effect="blur" />
-      {/* <LazyLoadImage src={concreteBauer} alt="" effect="blur" />
-      <LazyLoadImage src={concreteOGR} alt="" effect="blur" />
-      <LazyLoadImage src={concreteProphet} alt="" effect="blur" /> */}
+      <ReactImagesCarousel views={images} />
     </Container>
   );
 }
