dcae311187bc53c49e8537e645dd894475d9c6d9
Koen Romers <info@koenromers.com>
2020-01-14 20:29:32 +0100

    blur animation for lazyloading:

--- src/components/image.js
+++ src/components/image.js
@@ -4,18 +4,19 @@ import LazyLoad from 'react-lazyload';
 import styled from '@emotion/styled';
 import { keyframes } from '@emotion/core';
 
-const fadeIn = keyframes`
-  0% {
-    opacity: 0;
-  }
+// const fadeIn = keyframes`
+//   0% { opacity: 0; }
+//   100% { opacity: 1; }
+// `;
 
-  100% {
-    opacity: 1;
-  }
+const blur = keyframes`
+  0% { filter: blur(0px);}
+  50% { filter: blur(10px);}
+  100% { filter: blur(0px);}
 `;
 
 const Image = styled.img`
-  animation: ${fadeIn} 500ms ease;
+  animation: ${blur} 500ms ease;
 `;
 
 type Props = $ReadOnly<{|
