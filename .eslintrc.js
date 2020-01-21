c424c7c289a2909aa2295d6a5ad33ab3d69ce9a2
Koen Romers <info@koenromers.com>
2020-01-21 12:43:17 +0100

    remove old rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,16 +15,7 @@ module.exports = {
     },
     sourceType: 'module',
   },
-  rules: {
-    // 'prettier/prettier': ['error', prettierOptions],
-    // 'flowtype/generic-spacing': 0,
-    // 'import/extensions': ['error', 'always', { ignorePackages: true }],
-    // 'import/no-extraneous-dependencies': 0,
-    // 'import/prefer-default-export': 0,
-    // 'jsx-a11y/click-events-have-key-events': 0,
-    // 'react/jsx-filename-extension': 0,
-    // 'react/require-default-props': 0,
-  },
+  rules: {},
   settings: {
     'import/resolver': {
       webpack: {
